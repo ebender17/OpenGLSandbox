b337bdc01d67bfef77a980ccf93e9ac730c8684d
Emily Bender <36086595+ebender17@users.noreply.github.com>
2025-02-03 20:38:44 -0500

    Add Stencil Buffer Sandbox (#2)

---
+++ OpenGLSandbox/assets/shaders/textureunlit.vert.glsl
@@ -0,0 +1,15 @@
+#version 460
+
+layout (location = 0) in vec3 a_Position;
+layout (location = 1) in vec2 a_TexCoord;
+
+layout (location = 0) out vec2 v_TexCoords;
+
+uniform mat4 u_Model;
+uniform mat4 u_ViewProjection;
+
+void main()
+{
+    v_TexCoords = a_TexCoord;
+    gl_Position = u_ViewProjection * u_Model * vec4(a_Position, 1.0);
+}
