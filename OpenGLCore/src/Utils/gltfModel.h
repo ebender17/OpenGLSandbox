446ac83385667fc3f85f465b3b9e63822d5841f7
Emily Bender <bender.emily1@gmail.com>
2025-02-21 17:08:18 -0500

    Bind gltf model

--- OpenGLCore/src/Utils/gltfModel.h
+++ OpenGLCore/src/Utils/gltfModel.h
@@ -2,12 +2,20 @@
 
 #include <tiny_gltf.h>
 
+#include <glad/glad.h>
+
 namespace OpenGLCore::Utils {
 
     class gltfModel
     {
     public:
         static bool LoadModel(tinygltf::Model& model, const char* filename);
+
+        static std::pair<GLuint, std::map<int, GLuint>> BindModel(tinygltf::Model& model);
+
+    private:
+        static void BindModelNodes(std::map<int, GLuint>& vbos, tinygltf::Model& model, tinygltf::Node& node);
+        static void BindMesh(std::map<int, GLuint>& vbos, tinygltf::Model& model, tinygltf::Mesh& mesh);
     };
 
 }
